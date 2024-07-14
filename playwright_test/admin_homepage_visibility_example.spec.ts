bc8d257c5dba621c0623005a65bbea4f22983963
TheLastKura <114146886+TheLastKura@users.noreply.github.com>
2024-07-14 11:48:02 +0200

    Update admin_homepage_visibility_example.spec.ts

--- playwright_test/admin_homepage_visibility_example.spec.ts
+++ playwright_test/admin_homepage_visibility_example.spec.ts
@@ -2,6 +2,7 @@ import { test, expect } from "@playwright/test";
 
 test.describe("Check visibility of default functionalities for an admin user.", () => {
   test("admin_homepage_visibility", async ({ page }) => {
+    // Note: The login process and initial page navigation have been skipped as the focus is on showing a few Playwright functions.
     await expect(page.locator("[menu-list]")).toBeVisible;
     await expect(page.locator("[menu-list]")).toHaveScreenshot("listmenu.png");
     await expect(
